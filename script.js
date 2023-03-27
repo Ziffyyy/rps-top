610ec8f413022694adb0e080a2b8737523b5bad5
Ziffyyy <dreamofhallow@gmail.com>
2023-03-27 18:26:33 -0300

    Add the play round function
    For this function I needed to return the two values I was planning on
    using later on the game function, and I quickly found out that
    you can't. They say the best way would be to return the values inside an
    array but I tried wrapping them in a string and it appears to work.
    I also set the default values for the two score variables to 0 so the
    increment operators work as intended.
    I'm going to make the game function now and see if it all works.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 const possibleValues = ['rock', 'paper', 'scissors'];
 //Make the computerChoice, userChoice, userScore and computerScore variables
 let userChoice;
-let userScore;
+let userScore = 0;
 let computerChoice;
-let computerScore;
+let computerScore = 0;
 
 //Make the function that gets the computer choice
 function getComputerChoice() {
@@ -26,7 +26,22 @@ function getUserChoice() {
     }
 }
 //Make the function that takes the previous returns and compares them to get a winner
-
+function playRound(userChoice, computerChoice) {
+    userChoice = getUserChoice();
+    computerChoice = getComputerChoice();
+    if (userChoice === computerChoice) {
+        alert('It\'s a tie!');
+        return `Your score is: ${userScore}\n The machine score is: ${computerScore}.`;
+    } else if (userChoice === 'rock' && computerChoice === 'scissors' ||
+    userChoice === 'paper' && computerChoice === 'rock' ||
+    userChoice === 'scissors' && computerChoice === 'paper') {
+        alert('You won this round!')
+        return `Your score is: ${++userScore}\n The machine score is: ${computerScore}.`;
+    } else {
+        alert('The machine takes this one.')
+        return `Your score is: ${userScore}\n The machine score is: ${++computerScore}.`;
+    }
+}
 //Make a function that repeats the last one five times and returns a winner
 
 
