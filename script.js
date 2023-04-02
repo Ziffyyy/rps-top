0d075ff527986e3879b4199a95c33ee7476368be
Ziffyyy <dreamofhallow@gmail.com>
2023-04-02 16:45:44 -0300

    Add style
    Added a css file and gave style to the page, also added
    images to represent player and computer choices

--- script.js
+++ script.js
@@ -3,17 +3,23 @@ let userChoice;
 let userScore = 0;
 let computerChoice;
 let computerScore = 0;
+
 let isResetButtonAppended = false;
 let alreadyCalledWinner = false;
+
 const buttons = document.querySelectorAll('.choiceButton');
 const container = document.querySelector('#container');
-const resultContainer = document.querySelector('#results')
+const resultContainer = document.querySelector('#results');
+const userChoiceDisplay = document.querySelector('#user-choice');
+const computerChoiceDisplay = document.querySelector('#computer-choice');
+const displayContainers = document.querySelectorAll('.choices');
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         userChoice = button.id;
         computerChoice = getComputerChoice();
         playRound(userChoice,computerChoice);
+        displayChoice(userChoice,computerChoice);
         updateScoreText();
         if(userScore === 5 || computerScore === 5) {
             alert(winner(userScore, computerScore));
@@ -78,3 +84,11 @@ function winner() {
 
 }
 
+function displayChoice(userChoice, computerChoice) {
+    userChoiceDisplay.src = `images/${userChoice}.svg`;
+    computerChoiceDisplay.src = `images/${computerChoice}.svg`;
+    
+    displayContainers.forEach((container) => {
+        container.style = 'border: 5px solid #3d3c3b';
+    })
+}
